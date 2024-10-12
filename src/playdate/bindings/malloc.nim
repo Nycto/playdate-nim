0b77a522a6195545cf71b5105b067a411354a742
Nycto <James@RoundEights.com>
2024-10-11 21:57:47 -0700

    Use playdate realloc for SDK allocated pointers
    This resolves the memory tracer reporting calls to deallocate
    unmanaged memory

--- src/playdate/bindings/malloc.nim
+++ src/playdate/bindings/malloc.nim
@@ -18,50 +18,29 @@
 when defined(memProfiler):
     proc nimProfile(requestedSize: int)
 
-import memtrace
+import memtrace, initreqs
 import system/ansi_c
-
-type PDRealloc = proc (p: pointer; size: csize_t): pointer {.tags: [], raises: [], cdecl, gcsafe.}
-
-proc nativeAlloc(p: pointer; size: csize_t): pointer {.tags: [], raises: [], cdecl, gcsafe.} =
-    if p == nil:
-        return c_malloc(size)
-    elif size == 0:
-        c_free(p)
-        return nil
-    else:
-        return c_realloc(p, size)
-
-var pdrealloc: PDRealloc
 
 var trace: MemTrace
 
-
-proc setupRealloc*(allocator: PDRealloc) =
-    when defined(memtrace):
-        cfprintf(cstderr, "Setting up playdate allocator")
-    when defined(nativeAlloc):
-        pdrealloc = nativeAlloc
-    else:
-        pdrealloc = allocator
-
 proc allocImpl(size: Natural): pointer =
     # Integrage with: https://nim-lang.org/docs/estp.html
-    when defined(memProfiler):
-        {.cast(tags: []).}:
+    {.cast(tags: []).}:
+        when defined(memProfiler):
             try:
                 nimProfile(size.int)
             except:
                 discard
 
-    return trace.alloc(pdrealloc, size.csize_t)
+        return trace.alloc(pdrealloc, size.csize_t)
 
 proc alloc0Impl(size: Natural): pointer =
     result = allocImpl(size)
     zeroMem(result, size)
 
 proc reallocImpl(p: pointer, newSize: Natural): pointer =
-    return trace.realloc(pdrealloc, p, newSize)
+    {.cast(tags: []).}:
+        return trace.realloc(pdrealloc, p, newSize)
 
 proc realloc0Impl(p: pointer, oldsize, newSize: Natural): pointer =
     result = reallocImpl(p, newSize.csize_t)
@@ -69,7 +48,8 @@ proc realloc0Impl(p: pointer, oldsize, newSize: Natural): pointer =
         zeroMem(cast[pointer](cast[uint](result) + uint(oldSize)), newSize - oldSize)
 
 proc deallocImpl(p: pointer) =
-    trace.dealloc(pdrealloc, p)
+    {.cast(tags: []).}:
+        trace.dealloc(pdrealloc, p)
 
 # The shared allocators map on the regular ones
 
