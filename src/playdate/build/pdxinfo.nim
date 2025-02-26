82bef19f0a515fb76627a38fd6f216e7aad6aff8
Nycto <James@RoundEights.com>
2025-02-25 18:48:54 -0800

    Use Option for optional pdxinfo fields

--- src/playdate/build/pdxinfo.nim
+++ src/playdate/build/pdxinfo.nim
@@ -1,14 +1,17 @@
-import std/[os, parsecfg, streams, strutils, strformat, times, osproc], nimbledump
+import std/[os, parsecfg, streams, strutils, strformat, times, osproc, options], nimbledump
 
 type PdxInfo* = object
     ## Details used to populate the pdxinfo file
-    name*, author*, description*, bundleId*, imagePath*, version*: string
-    buildNumber*, launchSoundPath*, contentWarning*, contentWarning2*: string
+    name*, author*, description*, bundleId*, imagePath*, version*, buildNumber*: string
+    launchSoundPath*, contentWarning*, contentWarning2*: Option[string]
+
+proc isSome(value: string): bool = not value.isEmptyOrWhitespace
 
 proc `$`*(pdx: PdxInfo): string =
     for key, value in pdx.fieldPairs:
-        if value != "":
-            result &= key & "=" & value & "\n"
+        if value.isSome:
+            let str = when value is Option: value.get() else: value
+            result &= key & "=" & str & "\n"
 
 proc write*(pdx: PdxInfo) =
     ## Writes the pdxinfo file
@@ -19,14 +22,16 @@ proc join*(a, b: PdxInfo): PdxInfo =
     ## Combins two PdxInfo instances
     result = a
     for current, override in fields(result, b):
-        if override != "":
+        if override.isSome:
             current = override
 
 proc parsePdx*(data: Stream, filename: string): PdxInfo =
     ## Parses a pdx config from a string
     let dict = loadConfig(data, filename)
     for key, value in result.fieldPairs:
-        value = dict.getSectionValue("", key)
+        let raw = dict.getSectionValue("", key)
+        if raw != "":
+            value = when typeof(value) is Option: some(raw) else: raw
 
 proc readPdx*(path: string): PdxInfo =
     ## Creates a pdx by reading a local pxinfo file
